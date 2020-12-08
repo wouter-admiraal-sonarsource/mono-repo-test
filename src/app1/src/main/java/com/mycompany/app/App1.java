4cc522095c019c86105258511415e8147d60ff0d
Wouter Admiraal <45544358+wouter-admiraal-sonarsource@users.noreply.github.com>
2020-12-08 14:32:34 +0100

    Update App1.java

--- src/app1/src/main/java/com/mycompany/app/App1.java
+++ src/app1/src/main/java/com/mycompany/app/App1.java
@@ -11,13 +11,7 @@ public class App1
     public App1() {}
 
     public static void main(String[] args) {
-if (true) {
         System.out.println(new App1().getMessage());
-} else if (false) {
-        System.out.println(new App1().getMessage());
-} else {
-        System.out.println(new App1().getMessage());
-}
     }
 
     private final String getMessage() {
