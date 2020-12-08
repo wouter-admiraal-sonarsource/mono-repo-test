ebf8af4e6ed016ffbb95e3f07c66cdfc4307aafe
Wouter Admiraal <45544358+wouter-admiraal-sonarsource@users.noreply.github.com>
2020-12-08 11:58:15 +0100

    Update and rename App.java to App2.java

--- src/app2/src/main/java/com/mycompany/app/App2.java
+++ src/app2/src/main/java/com/mycompany/app/App2.java
@@ -3,20 +3,20 @@ package com.mycompany.app;
 /**
  * Hello world!
  */
-public class App
+public class App2
 {
 
     private final String message = "Hello World!";
 
-    public App() {}
+    public App2() {}
 
     public static void main(String[] args) {
 if (true) {
-        System.out.println(new App().getMessage());
+        System.out.println(new App2().getMessage());
 } else if (false) {
-        System.out.println(new App().getMessage());
+        System.out.println(new App2().getMessage());
 } else {
-        System.out.println(new App().getMessage());
+        System.out.println(new App2().getMessage());
 }
     }
 
