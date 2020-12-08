71394e4213cf1daf3b7ddd665b0b4f928f78e458
Wouter Admiraal <45544358+wouter-admiraal-sonarsource@users.noreply.github.com>
2020-12-08 11:57:43 +0100

    Update and rename app 1 classes

--- src/app1/src/test/java/com/mycompany/app/App1Test.java
+++ src/app1/src/test/java/com/mycompany/app/App1Test.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.*;
 /**
  * Unit test for simple App.
  */
-public class AppTest
+public class App1Test
 {
 
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
@@ -29,7 +29,7 @@ if (true) {
     @Test
     public void testAppConstructor() {
         try {
-            new App();
+            new App1();
         } catch (Exception e) {
             fail("Construction failed.");
         }
@@ -38,7 +38,7 @@ if (true) {
     @Test
     public void testAppMain()
     {
-        App.main(null);
+        App1.main(null);
         try {
             assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
